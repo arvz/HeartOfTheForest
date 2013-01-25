f01972c8945ad30d59264c809cb4a2c0b251f63a
unknown <alex@alexn.id.au>
2013-01-26 10:45:21 +1100

    More complicated measure generation

--- Assets/Scripts/BallSource.js
+++ Assets/Scripts/BallSource.js
@@ -7,34 +7,63 @@ var beatsPerMinute : float;
 // to beats.
 class Measure {
 	var beats : int; // how many beats in the bar
-	var notes : float[]; // offsets (in beats) of the notes
+	var notes : float[]; // durations (in beats) of the notes
+	var offsets : float[]; // offsets of notes
 
 	function Measure(b : int, n : Array) {
 		beats = b;
 		notes = n;
+		offsets = Offsets();
+	}
+
+	private function Offsets() : float[] {
+		var offsets = new float[notes.length];
+		var currentOffset : float = 0.0;
+		for (var i = 0; i < notes.length; ++i) {
+			offsets[i] = currentOffset;
+			currentOffset += notes[i];
+		}
+		Debug.Log("Notes: " + notes.length + ", off " + currentOffset);
+		if (currentOffset > beats) {
+			Debug.LogWarning("Bar is too long! " + currentOffset); 
+		}
+		return offsets;
 	}
 }
 
-// Hardcoded bar for testing
-private var bar = new Measure(4, [0.0, 1, 2.5, 3]);
+// Hardcoded measures for testing
+//private var bar = new Measure(4, Crotchets(4));
+private var measures : Array;
 
+private var currentMeasureIndex : int;
 private var currentMeasureStartTime : float;
 private var nextBallIndex : float;
 
 function Start () {
+	currentMeasureIndex = 0;
 	nextBallIndex = 0;
+
+	measures = new Array();
+	measures.Push(new Measure(4, Crotchets(4)));
+	measures.Push(new Measure(4, Crotchets(4)));
+	measures.Push(new Measure(4, Quavers(8)));
+	Debug.Log("Measures " + measures);
 }
 
 function Update () {
-	var barDuration = bar.beats / beatsPerMinute * 60;
+	var barDuration = (measures[currentMeasureIndex] as Measure).beats / beatsPerMinute * 60;
+
 	if (Time.time > currentMeasureStartTime + barDuration) {
+		// Measure has finished, move to the next.
+		currentMeasureIndex = (currentMeasureIndex + 1) % measures.length;
 		currentMeasureStartTime = Time.time;
 		nextBallIndex = 0;
 	}
+	var currentMeasure : Measure = measures[currentMeasureIndex] as Measure;
 
-	if (nextBallIndex < bar.notes.length) {
+	if (nextBallIndex < currentMeasure.offsets.length) {
 		var secondsPerBeat = 60 / beatsPerMinute;
-		var nextBallTime = currentMeasureStartTime + bar.notes[nextBallIndex] * secondsPerBeat;
+		var nextBallTime = currentMeasureStartTime + currentMeasure.offsets[nextBallIndex] * secondsPerBeat;
 		if (Time.time > nextBallTime) {
 			var b = GameObject.Find("Ball");
 			var newBall = Instantiate(b);
@@ -44,3 +73,18 @@ function Update () {
 	}
 }
 
+private function Crotchets(n : int) {
+  var notes = new float[n];
+	for (var i = 0; i < n; ++i) {
+		notes[i] = 1;
+	}
+	return notes;
+}
+
+private function Quavers(n : int) {
+  var notes = new float[n];
+	for (var i = 0; i < n; ++i) {
+		notes[i] = 0.5;
+	}
+	return notes;
+}
