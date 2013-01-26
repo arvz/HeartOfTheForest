54bf455e0a16a309d73ea5007b3fedee898177fe
unknown <alex@alexn.id.au>
2013-01-26 13:43:27 +1100

    Fixed trigger detection to use TriggerEnter

--- Assets/Scripts/TriggerScript.js
+++ Assets/Scripts/TriggerScript.js
@@ -1,20 +1,26 @@
 #pragma strict
 
-//private var forestController;
+private var isKeyDown : boolean;
+private var colliding = new Array();
 
 function Start () 
 {
-	//forestController = GetComponent(ForestController);
 }
 
-function OnTriggerStay(other : Collider)
+function Update () 
 {
-	if (Input.GetKeyDown("space")) 
-	{
-		// TODO: trigger some hit animation?
-		Destroy(other.gameObject);
+	isKeyDown = Input.GetKeyDown("space");
+	if (isKeyDown && colliding.length > 0) {
+		var collider : Collider = colliding.Pop();
+		Destroy(collider.gameObject);
+		RemoveColliding(collider);
 		GameObject.Find("ForestController").GetComponent(ForestController).Grow();
 	}
+}
+
+function OnTriggerEnter(other : Collider) 
+{
+	colliding.Push(other);
 }
 
 function OnTriggerExit(other : Collider) 
@@ -22,4 +28,14 @@ function OnTriggerExit(other : Collider)
 	// Destroy in 0.5 sec.
 	// TODO: some animation?
 	Destroy(other.gameObject, 0.5);
+	RemoveColliding(other);
+}
+
+function RemoveColliding(c : Collider)
+{
+	for (var i = 0; i < colliding.length; ++i) {
+		if (colliding[i] == c) {
+			colliding.RemoveAt(i);
+		}
+	}
 }
